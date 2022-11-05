84dee2c3aa3add948201f5aa918e9f0f18f5e515
ndinfotech <ndtechland@gmail.com>
2022-11-05 19:11:01 +0530

    jihuisdftgyhuj

--- lib/modules_view/hospital_view/hospital_signup_part_1/hospital_signup_view_part1.dart
+++ lib/modules_view/hospital_view/hospital_signup_part_1/hospital_signup_view_part1.dart
@@ -22,43 +22,45 @@ class HospitalSignup1 extends StatelessWidget {
           ])),
       child: Scaffold(
         backgroundColor: Colors.transparent,
-        body: SingleChildScrollView(
-          child: Stack(
-            clipBehavior: Clip.none,
-            children: [
-              Positioned(
-                top: size.height * 0.025,
-                //bottom: size.height * 0.64,
-                //left: -30,
-                right: -size.width * 0.006,
-                child: Padding(
-                  padding: const EdgeInsets.all(2.0),
-                  child: Container(
-                    height: size.height * 0.230,
-                    width: size.width * 0.45,
-                    decoration: BoxDecoration(
-                        //color: Colors.,
-                        borderRadius: BorderRadius.only(
-                          topRight: Radius.circular(20),
-                        ),
-                        image: DecorationImage(
-                            image: AssetImage(
-                              'lib/assets/background_stack_png/hospitall.png',
-                            ),
-                            fit: BoxFit.cover)),
+        body: SafeArea(
+          child: SingleChildScrollView(
+            child: Stack(
+              clipBehavior: Clip.none,
+              children: [
+                Positioned(
+                  top: size.height * 0.025,
+                  //bottom: size.height * 0.64,
+                  //left: -30,
+                  right: -size.width * 0.006,
+                  child: Padding(
+                    padding: const EdgeInsets.all(2.0),
+                    child: Container(
+                      height: size.height * 0.230,
+                      width: size.width * 0.45,
+                      decoration: BoxDecoration(
+                          //color: Colors.,
+                          borderRadius: BorderRadius.only(
+                            topRight: Radius.circular(20),
+                          ),
+                          image: DecorationImage(
+                              image: AssetImage(
+                                'lib/assets/background_stack_png/hospitall.png',
+                              ),
+                              fit: BoxFit.cover)),
+                    ),
                   ),
                 ),
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Hospital1HeadText(),
-                  Hospital1Credentials(),
-                  //HospitalSocial(),
-                  //Spacer(),
-                ],
-              ),
-            ],
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Hospital1HeadText(),
+                    Hospital1Credentials(),
+                    //HospitalSocial(),
+                    //Spacer(),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
--- lib/modules_view/hospital_view/hospital_sigup_step2/components/credentials.dart
+++ lib/modules_view/hospital_view/hospital_sigup_step2/components/credentials.dart
@@ -31,239 +31,242 @@ class Hospital2Credentials extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return Form(
-      key: _hospital_2_controller.hospital2formkey,
-      autovalidateMode: AutovalidateMode.onUserInteraction,
-      child: Padding(
-        padding: EdgeInsets.all(30),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(
-                //height: size.height * 0.02,
-                ),
+    return SafeArea(
+      child: Form(
+        key: _hospital_2_controller.hospital2formkey,
+        autovalidateMode: AutovalidateMode.onUserInteraction,
+        child: Padding(
+          padding: EdgeInsets.all(30),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                  //height: size.height * 0.02,
+                  ),
 
-            ///todo: phone number..........
-            NeumorphicTextFieldContainer(
-              child: TextFormField(
-                autofillHints: [AutofillHints.telephoneNumber],
-                controller: _hospital_2_controller.mobileController,
-                onSaved: (value) {
-                  _hospital_2_controller.mobile = value!;
-                },
-                validator: (value) {
-                  return _hospital_2_controller.validPhone(value!);
-                },
-                cursorColor: Colors.black,
-                obscureText: false,
-                decoration: InputDecoration(
-                  hintText: 'Confirm Phone',
-                  helperStyle: TextStyle(
-                    color: black.withOpacity(0.7),
-                    fontSize: 18,
+              ///todo: phone number..........
+              NeumorphicTextFieldContainer(
+                child: TextFormField(
+                  autofillHints: [AutofillHints.telephoneNumber],
+                  controller: _hospital_2_controller.mobileController,
+                  onSaved: (value) {
+                    _hospital_2_controller.mobile = value!;
+                  },
+                  validator: (value) {
+                    return _hospital_2_controller.validPhone(value!);
+                  },
+                  cursorColor: Colors.black,
+                  obscureText: false,
+                  decoration: InputDecoration(
+                    hintText: 'Confirm Phone',
+                    helperStyle: TextStyle(
+                      color: black.withOpacity(0.7),
+                      fontSize: 18,
+                    ),
+                    prefixIcon: Icon(
+                      Icons.phone_android_outlined,
+                      color: black.withOpacity(0.7),
+                      size: 20,
+                    ),
+                    border: InputBorder.none,
                   ),
-                  prefixIcon: Icon(
-                    Icons.phone_android_outlined,
-                    color: black.withOpacity(0.7),
-                    size: 20,
-                  ),
-                  border: InputBorder.none,
                 ),
               ),
-            ),
 
-            ///Todo: state............................
-            SizedBox(
-              height: size.height * 0.01,
-            ),
+              ///Todo: state............................
+              SizedBox(
+                height: size.height * 0.01,
+              ),
 
-            NeumorphicTextFieldContainer(
-              child: Padding(
-                padding: EdgeInsets.symmetric(horizontal: size.width * 0.01),
-                child: Obx(
-                  () => DropdownButtonFormField(
-                      value: _hospital_2_controller.selectedState.value,
-                      decoration: InputDecoration(
-                        prefixIcon: Icon(
-                          Icons.real_estate_agent,
-                          color: Colors.black,
+              NeumorphicTextFieldContainer(
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: size.width * 0.01),
+                  child: Obx(
+                    () => DropdownButtonFormField(
+                        value: _hospital_2_controller.selectedState.value,
+                        decoration: InputDecoration(
+                          prefixIcon: Icon(
+                            Icons.real_estate_agent,
+                            color: Colors.black,
+                          ),
+                          enabledBorder: InputBorder.none,
+                          border: InputBorder.none,
                         ),
-                        enabledBorder: InputBorder.none,
-                        border: InputBorder.none,
-                      ),
-                      hint: Text('Select State'),
-                      items: items.map((String items) {
-                        return DropdownMenuItem(
-                          value: items,
-                          child: Text(
-                            items,
-                            style: TextStyle(
-                              fontWeight: FontWeight.w600,
-                              fontSize: size.height * 0.015,
+                        hint: Text('Select State'),
+                        items: items.map((String items) {
+                          return DropdownMenuItem(
+                            value: items,
+                            child: Text(
+                              items,
+                              style: TextStyle(
+                                fontWeight: FontWeight.w600,
+                                fontSize: size.height * 0.015,
+                              ),
                             ),
-                          ),
-                        );
-                      }).toList(),
-                      onChanged: (String? newValue) {
-                        _hospital_2_controller.selectedState.value = newValue!;
-                        // _hospital_2_controller.states.value =
-                        //     newValue! as List<String>;
-                        // _hospital_2_controller.selectedCity.value = null;
-                        // _hospital_2_controller.cities.clear();
-                        // _hospital_2_controller.cities
-                        //     .addAll(stateCityMap[newvalue]!);
-                      }),
+                          );
+                        }).toList(),
+                        onChanged: (String? newValue) {
+                          _hospital_2_controller.selectedState.value =
+                              newValue!;
+                          // _hospital_2_controller.states.value =
+                          //     newValue! as List<String>;
+                          // _hospital_2_controller.selectedCity.value = null;
+                          // _hospital_2_controller.cities.clear();
+                          // _hospital_2_controller.cities
+                          //     .addAll(stateCityMap[newvalue]!);
+                        }),
+                  ),
                 ),
               ),
-            ),
 
-            ///Todo: city.....................................
+              ///Todo: city.....................................
 
-            SizedBox(
-              height: size.height * 0.02,
-            ),
+              SizedBox(
+                height: size.height * 0.02,
+              ),
 
-            NeumorphicTextFieldContainer(
-              child: Padding(
-                padding: EdgeInsets.symmetric(horizontal: size.width * 0.01),
-                child: Obx(
-                  () => DropdownButtonFormField(
-                      //icon: Icon(Icons.location_city),
-                      value: _hospital_2_controller.selectedCity.value,
-                      decoration: InputDecoration(
-                        prefixIcon: Icon(
-                          Icons.location_city,
-                          color: Colors.black,
+              NeumorphicTextFieldContainer(
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: size.width * 0.01),
+                  child: Obx(
+                    () => DropdownButtonFormField(
+                        //icon: Icon(Icons.location_city),
+                        value: _hospital_2_controller.selectedCity.value,
+                        decoration: InputDecoration(
+                          prefixIcon: Icon(
+                            Icons.location_city,
+                            color: Colors.black,
+                          ),
+                          enabledBorder: InputBorder.none,
+                          border: InputBorder.none,
                         ),
-                        enabledBorder: InputBorder.none,
-                        border: InputBorder.none,
-                      ),
-                      hint: Text('Selected City'),
-                      items: items.map((String items) {
-                        return DropdownMenuItem(
-                          value: items,
-                          child: Text(
-                            items,
-                            style: TextStyle(
-                              fontWeight: FontWeight.w600,
-                              fontSize: size.height * 0.015,
+                        hint: Text('Selected City'),
+                        items: items.map((String items) {
+                          return DropdownMenuItem(
+                            value: items,
+                            child: Text(
+                              items,
+                              style: TextStyle(
+                                fontWeight: FontWeight.w600,
+                                fontSize: size.height * 0.015,
+                              ),
                             ),
-                          ),
-                        );
-                      }).toList(),
-                      onChanged: (String? newValue) {
-                        _hospital_2_controller.selectedCity.value = newValue!;
-                        // _hospital_2_controller.states.value =
-                        //     newValue! as List<String>;
-                        // _hospital_2_controller.selectedCity.value = null;
-                        // _hospital_2_controller.cities.clear();
-                        // _hospital_2_controller.cities
-                        //     .addAll(stateCityMap[newvalue]!);
-                      }),
+                          );
+                        }).toList(),
+                        onChanged: (String? newValue) {
+                          _hospital_2_controller.selectedCity.value = newValue!;
+                          // _hospital_2_controller.states.value =
+                          //     newValue! as List<String>;
+                          // _hospital_2_controller.selectedCity.value = null;
+                          // _hospital_2_controller.cities.clear();
+                          // _hospital_2_controller.cities
+                          //     .addAll(stateCityMap[newvalue]!);
+                        }),
+                  ),
                 ),
               ),
-            ),
 
-            // child: DropdownButton(
-            //     value: _hospital_2_controller.selectedState.value,
-            //     menuMaxHeight: size.height * 0.3,
-            //     items: items.map((String items) {
-            //       return DropdownMenuItem(
-            //         value: items,
-            //         child: Text(items),
-            //       );
-            //     }).toList(),
-            //     // _hospital_2_controller.states.map((String value) {
-            //     //   return DropdownMenuItem(
-            //     //     value: value,
-            //     //
-            //     //   )
-            //     onChanged: (String? newValue) {
-            //       _hospital_2_controller.states.value =
-            //           newValue! as List<String>;
-            //       _hospital_2_controller.selectedCity.value = null;
-            //       _hospital_2_controller.cities.clear();
-            //       _hospital_2_controller.cities
-            //           .addAll(stateCityMap[newvalue]!);
-            //     })),
-            SizedBox(
-              height: size.height * 0.02,
-            ),
+              // child: DropdownButton(
+              //     value: _hospital_2_controller.selectedState.value,
+              //     menuMaxHeight: size.height * 0.3,
+              //     items: items.map((String items) {
+              //       return DropdownMenuItem(
+              //         value: items,
+              //         child: Text(items),
+              //       );
+              //     }).toList(),
+              //     // _hospital_2_controller.states.map((String value) {
+              //     //   return DropdownMenuItem(
+              //     //     value: value,
+              //     //
+              //     //   )
+              //     onChanged: (String? newValue) {
+              //       _hospital_2_controller.states.value =
+              //           newValue! as List<String>;
+              //       _hospital_2_controller.selectedCity.value = null;
+              //       _hospital_2_controller.cities.clear();
+              //       _hospital_2_controller.cities
+              //           .addAll(stateCityMap[newvalue]!);
+              //     })),
+              SizedBox(
+                height: size.height * 0.02,
+              ),
 
-            ///TODO: Pin.......................
-            NeumorphicTextFieldContainer(
-              child: TextFormField(
-                autofillHints: [AutofillHints.name],
-                controller: _hospital_2_controller.pinController,
-                onSaved: (value) {
-                  _hospital_2_controller.pin = value!;
-                },
-                validator: (value) {
-                  return _hospital_2_controller.validPin(value!);
-                },
-                cursorColor: Colors.black,
-                obscureText: false,
-                decoration: InputDecoration(
-                  hintText: 'Pin',
-                  helperStyle: TextStyle(
-                    color: black.withOpacity(0.7),
-                    fontSize: 18,
+              ///TODO: Pin.......................
+              NeumorphicTextFieldContainer(
+                child: TextFormField(
+                  autofillHints: [AutofillHints.name],
+                  controller: _hospital_2_controller.pinController,
+                  onSaved: (value) {
+                    _hospital_2_controller.pin = value!;
+                  },
+                  validator: (value) {
+                    return _hospital_2_controller.validPin(value!);
+                  },
+                  cursorColor: Colors.black,
+                  obscureText: false,
+                  decoration: InputDecoration(
+                    hintText: 'Pin',
+                    helperStyle: TextStyle(
+                      color: black.withOpacity(0.7),
+                      fontSize: 18,
+                    ),
+                    prefixIcon: Icon(
+                      Icons.pin,
+                      color: black.withOpacity(0.7),
+                      size: 20,
+                    ),
+                    border: InputBorder.none,
                   ),
-                  prefixIcon: Icon(
-                    Icons.pin,
-                    color: black.withOpacity(0.7),
-                    size: 20,
-                  ),
-                  border: InputBorder.none,
                 ),
               ),
-            ),
 
-            SizedBox(
-              height: size.height * 0.00,
-              //appPadding / 2,
-            ),
+              SizedBox(
+                height: size.height * 0.00,
+                //appPadding / 2,
+              ),
 
-            GetBuilder<Hospital_2_Controller>(
-              // specify type as Controller
-              init: Hospital_2_Controller(), // intialize with the Controller
-              builder: (value) => InkWell(
-                onTap: () {
-                  _hospital_2_controller.getImage(ImageSource.gallery);
-                },
-                child: NeumorphicTextFieldContainer(
-                  child: Container(
-                    height: size.height * 0.057,
-                    //width: size.width * 0.5,
-                    child: Padding(
-                      padding:
-                          EdgeInsets.symmetric(horizontal: size.width * 0.1),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            'Authorise Letter Image',
-                            style: TextStyle(
-                              fontSize: size.width * 0.03,
-                              fontWeight: FontWeight.w700,
+              GetBuilder<Hospital_2_Controller>(
+                // specify type as Controller
+                init: Hospital_2_Controller(), // intialize with the Controller
+                builder: (value) => InkWell(
+                  onTap: () {
+                    _hospital_2_controller.getImage(ImageSource.gallery);
+                  },
+                  child: NeumorphicTextFieldContainer(
+                    child: Container(
+                      height: size.height * 0.06,
+                      //width: size.width * 0.5,
+                      child: Padding(
+                        padding:
+                            EdgeInsets.symmetric(horizontal: size.width * 0.1),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              'Authorise Letter Image',
+                              style: TextStyle(
+                                fontSize: size.width * 0.03,
+                                fontWeight: FontWeight.w700,
+                              ),
                             ),
-                          ),
-                          Icon(Icons.camera),
-                        ],
+                            Icon(Icons.camera),
+                          ],
+                        ),
                       ),
                     ),
                   ),
                 ),
               ),
-            ),
 
-            RectangularButton(
-                text: 'SUBMIT',
-                press: () {
-                  Get.to(SignUpList());
-                  //_loginpasswordController.checkLoginpassword();
-                })
-          ],
+              RectangularButton(
+                  text: 'SUBMIT',
+                  press: () {
+                    Get.to(SignUpList());
+                    //_loginpasswordController.checkLoginpassword();
+                  })
+            ],
+          ),
         ),
       ),
     );
